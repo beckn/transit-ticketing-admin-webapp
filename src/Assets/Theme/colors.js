9e009ccb64d357528b18ab8ae463f1a899ab35fd
VarunDev <varundev76>
2022-03-30 02:01:34 +0530

    [Varun] [Add] add login Stuff All

---
+++ src/Assets/Theme/colors.js
@@ -0,0 +1,23 @@
+const Dim_Black ="#424242";
+const Grey_Black = "#3E4059";
+const White = "#ffff";
+const Beige = "#F3F1E0";
+const Black = "#000000";
+const Off_Black = "#33393C";
+const Dim_Grey = "#F0F0F0";
+const Disabled_Button = "#AAABB5";
+const Transparent = "transparent";
+const Button_Gradient = "linear-gradient(180deg, #D27F65 0%, #F8A388 100%)";
+
+export const colors = {
+  Dim_Black,
+  Grey_Black,
+  White,
+  Beige,
+  Black,
+  Dim_Grey,
+  Disabled_Button,
+  Transparent,
+  Off_Black,
+  Button_Gradient
+};
--- src/Pages/Auth/login.tsx
+++ src/Pages/Auth/login.tsx
@@ -1,28 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import {
   Button,
   Checkbox,
   Flex,
   FormControl,
-  FormLabel,
   Heading,
   Input,
   Stack,
   Image,
+  FormErrorMessage,
+  useToast
 } from "@chakra-ui/react";
-import { Redirect, useHistory } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import { useHistory } from "react-router-dom";
 import logging from "../../Config/logging";
 import { auth } from "../../Config/firebase";
-import AuthContainer from "../../Components/AuthRoute/index";
+// import AuthContainer from "../../Components/AuthRoute/index";
 import IPageProps from "../../Interfaces/page";
 import { getLocalStorage, setLocalStorage } from "../../utils/helpers";
+import { colors } from "../../Assets/Theme/colors";
+import IMAGE_PREFIX from "../../Config/image";
 
-const LoginScreen: React.FC<IPageProps> = (props) => {
-  const [authenticating, setAuthenticating] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid Email').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+})
+
+const LoginScreen: React.FC<IPageProps> = (props): ReactElement => {
+  const toast = useToast();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: LoginSchema,
+    onSubmit: ( values, actions ) => {
+      if(values.email === "" || values.password === "") return;
+      signInWithEmailAndPassword(values.email, values.password);
+      actions.resetForm();
+    }
+  });
   const history = useHistory();
 
   useEffect(() => {
@@ -36,10 +56,7 @@ const LoginScreen: React.FC<IPageProps> = (props) => {
     }
   },[]);
 
-  const signInWithEmailAndPassword = () => {
-    if (error !== "") setError("");
-
-    setAuthenticating(true);
+  const signInWithEmailAndPassword = (email: string, password: string) => {
 
     auth.signInWithEmailAndPassword(email, password)
     .then(result => {
@@ -49,83 +66,134 @@ const LoginScreen: React.FC<IPageProps> = (props) => {
     })
     .catch(error => {
         logging.error(error);
-        setAuthenticating(false);
-        setError(error.message);
+        toast({
+          title: 'Account Issue.',
+          description: error.message,
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        })
     });
   }
 
   return (
     // <AuthContainer>
       <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
-        <Flex flex={1} align="center" justify="center">
-          <Flex direction="column">
-            <Heading fontSize={"3xl"} textAlign="left">Government of Kerala</Heading>
-            <Heading 
-              fontSize={"5xl"} 
-              fontWeight="300"
-              mt="4" 
-              textAlign="left"
-            >
-              State Water Transport <br /> Department
-            </Heading>
-          </Flex>
-        </Flex>
 
         <Flex p={8} flex={1} align={"center"} justify={"center"}>
-          <Stack spacing={4} border={"1px solid #000000"} pt={"49"} w={"full"} maxW={"md"}>
-            {/* <Heading fontSize={"2xl"}>Sign in to your account</Heading> */}
-            <Flex flex={1} justify="center">
-              <Image
-                alt={"Login Image"}
-                borderRadius="full"
-                boxSize="227px"
-                objectFit={"cover"}
-                src={
-                  "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
-                }
-              />
-            </Flex>
-            <FormControl px={"10"} id="email">
-              <FormLabel>User ID</FormLabel>
+          <Stack 
+            spacing={8} 
+            background={colors.Grey_Black}
+            borderRadius={"2xl"}
+            py={"49"} 
+            w={"full"} 
+            maxW={"md"}
+            as="form"
+            // onSubmit={() => formik.handleSubmit()}
+          >
+            <Heading px={"6"} color={colors.White} fontSize={"4xl"}>State Water Transport Department</Heading>
+            
+            <FormControl px={"6"} id="email" isInvalid={formik.errors.email && formik.touched.email ? true : false}>
               <Input 
                 type="email" 
                 name="email"
                 id="email"
-                placeholder="Email Address"
-                onChange={event => setEmail(event.target.value)}
-                value={email}
+                placeholder="Enter Username"
+                color={colors.White}
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                fontSize={"2xl"}
+                size={"78px"}
+                p={"5"}
+                borderRadius= "24px"
+                _placeholder={{ opacity: 1, color: `${colors.White}.600` }}
+                onBlur={formik.handleBlur}
+                focusBorderColor={`${colors.White}.400`}
               />
+              <FormErrorMessage> {formik.errors.email} </FormErrorMessage>
             </FormControl>
-            <FormControl px={"10"} id="password">
-              <FormLabel>Password</FormLabel>
+
+            <FormControl px={"6"} id="password" isInvalid={formik.errors.password && formik.touched.password ? true : false}>
               <Input 
                 autoComplete="new-password"
                 type="password"
                 name="password"
                 id="password"
+                color={colors.White}
                 placeholder="Enter Password"
-                onChange={event => setPassword(event.target.value)}
-                value={password}
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                fontSize={"2xl"}
+                size={"78px"}
+                p={"5"}
+                borderRadius= "24px"
+                _placeholder={{ opacity: 1, color: `${colors.White}.600` }}
+                focusBorderColor={`${colors.White}.400`}
+                onBlur={formik.handleBlur}
               />
+              <FormErrorMessage> {formik.errors.password} </FormErrorMessage>
             </FormControl>
-            <Stack spacing={6}>
+
+            <Stack spacing={6} px={"10"}>
               <Stack
-                px={"10"}
+                color={colors.White}
                 direction={{ base: "column", sm: "row" }}
                 align={"start"}
                 justify={"space-between"}>
                 <Checkbox>Remember me</Checkbox>
               </Stack>
+
               <Button 
-                colorScheme={"blue"} 
+                type="button"
+                mx={"10"}
+                height={"78px"}
+                borderRadius={"24px"}
                 variant={"solid"}
-                disabled={authenticating}
-                onClick={() => signInWithEmailAndPassword()}
+                disabled={formik.isSubmitting}
+                color={colors.White}
+                bgGradient={`linear(to-r, #D27F65, #F8A388)`}
+                _hover={{
+                  bgGradient: `linear(to-r, #D27F65, #F8A388)`
+                }}
+                onClick={() => formik.handleSubmit()}
               >
                 Submit
               </Button>
             </Stack>
           </Stack>
+        </Flex>
+
+
+        <Flex p={8} flex={1} align="center" justify="center" position={"relative"}>
+          <Flex direction="column" zIndex={"-1"} width={"full"} position={"relative"} height={"100%"} maxH={"728px"}>
+            <Flex direction="column" align={"center"} mt={"8%"} justify="center" width={"full"}>
+              <Image
+                alt={"Logo Image"}
+                borderRadius="full"
+                boxSize="227px"
+                objectFit={"contain"}
+                src={IMAGE_PREFIX.Logo}
+              />
+              <Heading fontSize={"2xl"} textAlign="left">Government of Kerala</Heading>
+            </Flex>
+
+            <Flex 
+              width={"60vw"}
+              minW={"1077px"}
+              position={"absolute"} 
+              right={"0%"}
+              bottom={"15%"}
+            >
+              <Image
+                alt={"Logo Image"}
+                borderRadius="full"
+                // boxSize="227px"
+                // style={{ position: "absolute", right: 0 }}
+                objectFit={"contain"}
+                src={IMAGE_PREFIX.BoatIcon}
+              />
+            </Flex>
+          </Flex>
         </Flex>
       </Stack>
     // </AuthContainer>
